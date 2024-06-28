74bdeb493e70c14dc94f64bf10c4c11f5c1f53d0
AbayoHJovin <abayohirwajovin@gmail.com>
2024-06-28 11:24:01 +0200

    Added not found page

---
+++ view/proj/src/components/notFound.jsx
@@ -0,0 +1,24 @@
+import { useNavigate } from "react-router-dom";
+
+const NotFound = () => {
+  document.title = "Artify-Not found";
+  const navigate = useNavigate();
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-red-600 mb-4">
+        404 - Page Not Found
+      </h1>
+      <p className="text-lg text-gray-700 mb-4">
+        Sorry, the page you are looking for does not exist.
+      </p>
+      <button
+        onClick={() => navigate("/")}
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300"
+      >
+        Go to Home
+      </button>
+    </div>
+  );
+};
+
+export default NotFound;
--- view/proj/src/main.jsx
+++ view/proj/src/main.jsx
@@ -6,6 +6,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Saved from "./components/saved.jsx";
 import Start from "./components/details.jsx";
 import Landing from "./components/landing.jsx";
+import NotFound from "./components/notFound.jsx";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,7 @@ const router = createBrowserRouter([
   },
   { path: "/details", element: <Start /> },
   { path: "/home", element: <MediaForm /> },
+  {path:"*",element:<NotFound/>}
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
